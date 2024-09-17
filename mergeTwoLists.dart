42b95446770cd83eea889ca693a218dd1ef2b705
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-09-17 22:24:43 +0500

    mere two lists

---
+++ mergeTwoLists.dart
@@ -0,0 +1,16 @@
+
+void main() {
+  // // Creating first linked list: 1 -> 2 -> 4
+  // ListNode list1 = ListNode(1, ListNode(2, ListNode(4)));
+  
+  // // Creating second linked list: 1 -> 3 -> 4
+  // ListNode list2 = ListNode(1, ListNode(3, ListNode(4)));
+
+  // // Merging the lists
+  // ListNode? mergedList = mergeTwoLists(list1, list2);
+
+  // // Print merged list
+  // printList(mergedList);  // Output: 1 -> 1 -> 2 -> 3 -> 4 -> 4
+
+  print(2/4);
+}
